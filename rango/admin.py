4a8dff059a51e2a2d51790df980299f6165c0169
Ibad Khan <ibad@hpe.com>
2016-07-24 15:19:05 +0530

    Feature to modify category and page admin view

--- rango/admin.py
+++ rango/admin.py
@@ -2,5 +2,13 @@ from django.contrib import admin
 from rango.models import Category, Page
 
 
-admin.site.register(Category)
-admin.site.register(Page)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'views', 'likes')
+
+
+class PageAdmin(admin.ModelAdmin):
+    list_display = ('title', 'category', 'url')
+
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Page, PageAdmin)
