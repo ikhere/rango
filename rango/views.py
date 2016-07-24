6b9748c726596dee4b7d1d5357316ed95b1cd2ab
Ibad Khan <ibad@hpe.com>
2016-07-24 19:36:47 +0530

    Feature to show details of category with the help of slug field

--- rango/views.py
+++ rango/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from rango.models import Category
+from rango.models import Page
 
 
 def index(request):
@@ -13,3 +14,18 @@ def about(request):
     """Information about rango"""
     context = {'message': 'about'}
     return render(request, 'rango/about.html', context)
+
+
+def category(request, category_name_slug):
+    """Details about category"""
+    context = {}
+    try:
+        category = Category.objects.get(slug=category_name_slug)
+        context['category_name'] = category.name
+        pages = Page.objects.filter(category=category)
+        context['pages'] = pages
+        context['category'] = category
+    except Category.DoesNotExist:
+        pass
+
+    return render(request, 'rango/category.html', context)
