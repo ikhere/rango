0250172ad925d5b13beaacf8f7cd2a61ed7ddf72
Ibad Khan <ibad@hpe.com>
2016-07-24 15:20:02 +0530

    Script to populate category and page model with data

--- runserver.sh
+++ runserver.sh
@@ -1,8 +1,23 @@
 #!/bin/bash
 
+# Add /code folder to PYTHONPATH
 export PYTHONPATH=/code:$PYTHONPATH
 
-sleep 10	# Hack; will help web service to wait for db service.
+# Hack; will help web service to wait for db service.
+sleep 10
+
+# Enable this to make new migrations
+python manage.py makemigrations
+
+# Apply the migrations
 python manage.py migrate
+
+# Create superuser
 python manage.py initadmin
+
+# Start django server on port 8000
 python manage.py runserver 0.0.0.0:8000
+
+# Populate Rango database
+# NOTE: Disable once you've populated it
+# python populate_rango.py
