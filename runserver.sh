6bfe0863c48fa417fdcb5537ea3dc57fc2d4b15c
Ibad Khan <ibad@hpe.com>
2016-07-24 17:56:05 +0530

    Feature to add slug field in category model so that it's easy to form URL

--- runserver.sh
+++ runserver.sh
@@ -9,15 +9,18 @@ sleep 10
 # Enable this to make new migrations
 python manage.py makemigrations
 
+# Enable this to squash the migrations
+# python manage.py squashmigrations rango 0004 --noinput
+
 # Apply the migrations
-python manage.py migrate
+python manage.py migrate --noinput
 
 # Create superuser
 python manage.py initadmin
 
-# Start django server on port 8000
-python manage.py runserver 0.0.0.0:8000
-
 # Populate Rango database
 # NOTE: Disable once you've populated it
-# python populate_rango.py
+python populate_rango.py
+
+# Start django server on port 8000
+python manage.py runserver 0.0.0.0:8000
